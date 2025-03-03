89bce041e515b8135b083910fd581d8c488f772c
marykhodaparast <maryam.kh1374@gmail.com>
2025-03-03 15:41:38 +0330

    First commits -> create some example pages

---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        labore ad blanditiis natus sed ea autem culpa dolor esse necessitatibus
+        sint dolore debitis nobis optio porro, officiis deleniti reiciendis
+        odit!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
