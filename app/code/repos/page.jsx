56dcd68a3d0482eb5bba9a0e327513da0aa96749
marykhodaparast <maryam.kh1374@gmail.com>
2025-03-05 12:41:43 +0330

    work on repos page

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Link from "next/link";
+import { FaStar, FaCode, FaEye } from "react-icons/fa";
 
-const ReposPage = () => {
-  return (
-    <div>ReposPage</div>
-  )
+async function fetchRepos() {
+  const response = await fetch('https://api.github.com/users/marykhodaparast/repos');
+  const repos = await response.json();
+  return repos;
 }
 
-export default ReposPage
+const ReposPage = async () => {
+  const repos = await fetchRepos();
+  console.log(repos)
+
+  return <div className="repos-container">
+    <h2>Repositories</h2>
+    <ul className="repo-list">
+      {repos.map((repo) => {
+        <li key={repo.id}>
+          <Link href={repo.html_url}></Link>
+        </li>
+      })}
+    </ul>
+  </div>;
+};
+
+export default ReposPage;
