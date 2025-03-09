a147d2d26eddfe0311aa01af4019ac0cd31aaff2
marykhodaparast <maryam.kh1374@gmail.com>
2025-03-09 15:50:28 +0330

    complete adding search and showing courses from api and use client

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,37 @@
+"use client";
+import { useState, useEffect } from "react";
 import Courses from "./components/Courses";
+import LoadingPage from './loading';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
       <h1>Welcome to Traversy Media</h1>
-      <Courses/>
+      <CourseSearch getSearchResults={(results) => {
+        setCourses(results);
+      }}/>
+      <Courses courses={courses} />
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
